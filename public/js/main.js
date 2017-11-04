c89f00b529252eef567a7e189f160e773d2428eb
Jordan Walker <jorwalk@gmail.com>
2017-11-04 13:59:38 -0500

    town hall

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-    console.log('HELLO')
     var map = new ol.Map({
         target: 'map',
         layers: [
             new ol.layer.Tile({
-                source: new ol.source.OSM()
+                source: new ol.source.XYZ({
+                    url: 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9yZGFud2Fsa2VyIiwiYSI6ImNpb2Q4YjNrbjA0aGh2Z2txc2V4c2RsYnUifQ.v3q_DsYnHTPVICPHaTWy5Q'
+                })
             })
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat(['0','0']),
+            center: ol.proj.fromLonLat(['0', '0']),
             zoom: 5
         })
     });
-
-    map.getView().setCenter(ol.proj.transform(['38.6270', '90.1994'],'EPSG:3857', 'EPSG:4326'))
-
+    map.getView().setCenter(ol.proj.transform(['38.6270', '-90.1994'], 'EPSG:3857', 'EPSG:4326'))
 });
