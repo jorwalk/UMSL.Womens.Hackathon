aff7fa3a2f84029914efc1f460cf1f55e97ab47d
Jordan Walker <jorwalk@gmail.com>
2017-11-04 14:34:10 -0500

    awareness

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
-    var map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.XYZ({
-                    url: 'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9yZGFud2Fsa2VyIiwiYSI6ImNpb2Q4YjNrbjA0aGh2Z2txc2V4c2RsYnUifQ.v3q_DsYnHTPVICPHaTWy5Q'
-                })
-            })
-        ],
-        view: new ol.View({
-            center: ol.proj.fromLonLat(['0', '0']),
-            zoom: 5
-        })
+    // var map = new ol.Map({
+    //     target: 'map',
+    //     projection:"EPSG:4326",
+    //     layers: [
+    //         new ol.layer.Tile({
+    //             source: new ol.source.XYZ({
+    //                 url: 'https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9yZGFud2Fsa2VyIiwiYSI6ImNpb2Q4YjNrbjA0aGh2Z2txc2V4c2RsYnUifQ.v3q_DsYnHTPVICPHaTWy5Q'
+    //             })
+    //         })
+    //     ],
+    //     view: new ol.View({
+    //         center: ol.proj.transform(['38.565909', '-90.239566'],'EPSG:3857','EPSG:4326'),
+    //         zoom: 5
+    //     })
+    // });
+    // console.log(map.getView().getProjection())
+    // map.getView().setCenter(ol.proj.transform(['894672.5', '995003.7'], , ))
+    $('#awarenessZipcode').keyup(function() {
+        if ($(this).val() == '63119') {
+            $('#awarenessContainer').show();
+        } else {
+            $('#awarenessContainer').hide();
+        }
     });
-    map.getView().setCenter(ol.proj.transform(['38.6270', '-90.1994'], 'EPSG:3857', 'EPSG:4326'))
 });
--- server.js
+++ server.js
@@ -8,6 +8,88 @@ server.connection({
   port: process.env.PORT || 5000
 })
 /* ROUTES */
+
+const awareness = {
+	"officials": [{
+		"title": "Mayor",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/mayor/profiles/images/Krewson-Official-Photo-Profile-250px.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/mayor/profiles/profile-mayor-lyda-krewson.cfm",
+			"name": "Lyda Krewson"
+		}
+	}, {
+		"title": "Comptroller",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/comptroller/profiles/images/Darlene-Green_2-250px_2.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/comptroller/profiles/About-Comptroller-Darlene-Green.cfm",
+			"name": "Darlene Green"
+		}
+	}, {
+		"title": "President of the Board of Aldermen",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/aldermen/profiles/images/Copy-of-Reed_Lewis_6275.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/aldermen/profiles/profile-president-lewis-reed.cfm",
+			"name": "Lewis Reed"
+		}
+	}, {
+		"title": "Sheriff",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/sheriff/profiles/images/vernon-betts-250px.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/sheriff/profiles/vernon-betts.cfm",
+			"name": "Vernon Betts",
+			"phone": "(314) 622-4131"
+		}
+	}, {
+		"title": "Recorder Of Deeds",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/recorder/profiles/images/Sharon_Quigley_Carpenter_4.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/recorder/profiles/recorder-of-deeds-sharon-quigley-carpenter.cfm",
+			"name": "Sharon Carpenter",
+			"phone": "(314) 622-4610"
+		}
+	}, {
+		"title": "Collector of Revenue",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/collector/profiles/images/Gregory-F-X-Daly-Portrait-250px.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/collector/profiles/Gregory-fx-daly.cfm",
+			"name": "Gregory F.X. Daly",
+			"phone": "(314) 622-4111"
+		}
+	}, {
+		"title": "Circuit Attorney",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/circuit-attorney/profiles/images/kimberly-gardner-circuit-attorney-250px.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/circuit-attorney/profiles/kim-gardner.cfm",
+			"name": "Kim Gardner",
+			"phone": "(314) 622-4941"
+		}
+	}, {
+		"title": "Treasurer, City of St. Louis",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/treasurer/images/Treasurer-Tishaura-Jones_2_4-250px.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/treasurer/profiles/Tishaura-Jones.cfm",
+			"name": "Tishaura O. Jones",
+			"phone": "(314) 622-4700"
+		}
+	}, {
+		"title": "License Collector",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/license/profiles/images/MavisTThompsonHeadShot-WR-131224.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/license/profiles/Mavis-T-Thompson-Esq.cfm",
+			"name": "Mavis Thompson",
+			"phone": "(314) 622-4528"
+		}
+	}, {
+		"title": "Ward 01 Alderwoman",
+		"data": {
+			"image": "https://www.stlouis-mo.gov/government/departments/aldermen/profiles/images/Tyus_Sharon_8491.jpg",
+			"cfm": "https://www.stlouis-mo.gov/government/departments/aldermen/profiles/alderman-sharon-tyus.cfm",
+			"name": "Sharon Tyus",
+			"phone": "(314) 622-3287"
+		}
+	}]
+}
+
 server.route([{
   method: 'GET',
   path: '/',
@@ -33,7 +115,7 @@ server.route([{
   method: 'GET',
   path: '/awareness',
   handler: function (request, reply) {
-    var data = { message: 'Put awareness here', title: 'Awareness' }
+    var data = { message: 'Put awareness here', title: 'Awareness', data: awareness }
     reply.view('awareness', data)
   }
 }])
